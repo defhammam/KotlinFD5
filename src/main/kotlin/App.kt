37081c4967aa7eefca452afda62f09ac5a541dd9
Hammam <hammam.mahfuzh.s@mail.ugm.ac.id>
2022-08-31 19:42:02 +0700

    Initial commit

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,10 @@
+class App {
+    fun run() {}
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val app = App()
+            app.run()
+        }
+    }
+}
